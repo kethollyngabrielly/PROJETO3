2563d348d07dc45073c388f479d8ddd60cf284ae
kethollyngabrielly <0000108938158xsp@al.educacao.sp.gov.br>
2024-08-30 19:19:52 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let cor;
+let cx;
+let cy;
+
+function setup() {
+ createCanvas(1000,800);
+  background ("#673AB7")
+  cor = color (random(0,255),random(0,255),random(0,255));
+  cy = [random(height), random(height), random(height)];
+  cx = [0,0,0];
+}
+  function draw() {
+  fill(cor);
+  for (let quantos in cx) {
+    circle(cx[quantos], cy[quantos], 50);
+    cx[quantos] += random(0, 3);
+    cy[quantos] += random(-3, 3);
+    
+    if(cx[quantos] >= width){
+      cx[quantos] = 0;
+      cy[quantos] = random(height);
+    }
+  }
+    if (mouseIsPressed){
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+}
